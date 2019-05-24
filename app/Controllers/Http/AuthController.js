8a6c8f189635634b6bfa8fb7678c552ac05314f2
Adriano Faria Alves <adriano.faria@outlook.com.br>
2019-05-24 10:33:33 -0300

    Verificando se nome de usuário e email é unico

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const User = use('App/Models/User');
-const { validateAll } = use('Validator');
+const User = use("App/Models/User");
+const { validateAll } = use("Validator");
 
 class AuthController {
   async login({ request, auth, response }) {
     const rules = {
-      email: 'required|email',
-      password: 'required'
+      email: "required|email",
+      password: "required"
     };
     const validation = await validateAll(request.all(), rules);
 
@@ -17,7 +17,7 @@ class AuthController {
 
     const { email, password } = request.all();
     const tokenObject = await auth.attempt(email, password);
-    const user = await User.findBy('email', email);
+    const user = await User.findBy("email", email);
     const { token } = tokenObject;
     const resp = { user: { username: user.username, email: user.email }, token: token };
 
@@ -31,10 +31,33 @@ class AuthController {
     return { id, username, email, created_at, updated_at };
   }
 
-  async signup({ request }) {
-    const data = request.only(['username', 'email', 'password']);
+  async signup({ request, response }) {
+    const rules = {
+      username: "required|unique:users,username",
+      email: "required|email|unique:users,email",
+      password: "required"
+    };
+    const validation = await validateAll(request.all(), rules);
+
+    if (validation.fails()) {
+      return response.status(422).json(validation.messages());
+    }
+
+    const data = request.only(["username", "email", "password"]);
     const user = await User.create(data);
 
+    return user;
+  }
+
+  async findByUsername({ params }) {
+    const { username } = params;
+    const user = await User.findByOrFail("username", username);
+    return user;
+  }
+
+  async findByEmail({ params }) {
+    const { email } = params;
+    const user = await User.findByOrFail("email", email);
     return user;
   }
 }
